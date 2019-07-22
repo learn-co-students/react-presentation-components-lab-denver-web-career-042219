1a62fdf42158e800aca5421bba0f5c304991fc57
drbarq <J.Tustin@gmail.com>
2019-07-22 13:41:12 -0600

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,29 @@
 // Code SimpleComponent Here
+import React from 'react'
+
+class SimpleComponent extends React.Component { 
+    constructor() {
+        super()
+        this.state = {
+            mood: "happy"
+        }
+    }
+
+    flipMood = (event) => {
+        this.state.mood !== "happy" ? this.setState({mood: "happy"}) : this.setState({mood: "sad"})
+    }
+
+    render() {
+        return(
+            <React.Fragment>
+                <div onClick={event => this.flipMood(event)}>
+                    {this.state.mood}
+                </div>
+            </React.Fragment>
+        )
+    }
+
+
+}
+
+export default SimpleComponent
